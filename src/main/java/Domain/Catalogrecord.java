6240efd7e95658a72df9156ae3ea9afcdd88ccd8
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2021-02-18 22:00:21 -0500

    added  classess

--- src/main/java/Domain/Catalogrecord.java
+++ src/main/java/Domain/Catalogrecord.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.	
  */	
 package Domain;	
-import Domain.Person;	
 import java.io.Serializable;	
 import javax.persistence.Column;	
 import javax.persistence.Entity;	
---
+++ src/main/java/Domain/Documenttype.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+import Domain.Person;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+/**
+ *
+ * @author Adrian
+ */
+public class Documenttype {
+    
+}
