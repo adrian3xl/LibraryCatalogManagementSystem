f41f5677bfa447cd83c1216ff00aa2a56265d0f1
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2021-02-18 22:19:26 -0500

    added  classess

--- src/main/java/Domain/Documenttype.java
+++ src/main/java/Domain/Documenttype.java
@@ -4,15 +4,42 @@
  * and open the template in the editor.
  */
 package Domain;
-import Domain.Person;
-import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 /**
  *
  * @author Adrian
  */
+
+@Entity	
+@Table(name="documenttype")
 public class Documenttype {
-    
+  
+      @Id	
+    @Column(name="name")	
+    private String name;	
+
+    public Documenttype( String name) {	
+        this.name=name;
+  
+    }	
+
+
+    public Documenttype(){	
+    }	
+
+    public void setName(String name) {	
+        this.name = name;	
+    }	
+
+    public String getName() {	
+        return name;	
+    }	
+
+   @Override	
+    public String toString() {	
+        return "Documenttype{" + "name=" + name + '}';	
+    }	
 }
