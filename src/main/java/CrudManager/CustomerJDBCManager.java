dc60bd9e837edbf0d46ebfd446adc94da3f9176f
adrian3xl <59806075+adrian3xl@users.noreply.github.com>
2021-03-09 15:31:18 -0500

    add logging to various function

--- src/main/java/CrudManager/CustomerJDBCManager.java
+++ src/main/java/CrudManager/CustomerJDBCManager.java
@@ -12,15 +12,18 @@ import Service.ICustomerService;
 import Service.Exceptions.ServiceLoadException;
 import Service.ICustomerServiceJDBC;
 import java.sql.ResultSet;
+import org.apache.log4j.Logger;
 /**
  *
  * @author Adrian
  */
 public class CustomerJDBCManager {
+       final static Logger logger = Logger.getLogger(CustomerJDBCManager.class); 
     
 public void addCustomerJDBC(Customer anCustomer)
     {
         try {
+            logger.info("In Business Layer addCustomer(Customer Customer) method");
             System.out.println("In Business Layer addCustomer(Customer Customer) method");
             Factory factory = new Factory();            
             ICustomerServiceJDBC iCustomerMgrJDBC = (ICustomerServiceJDBC) factory.getTheService(ICustomerServiceJDBC.NAME);
@@ -32,12 +35,14 @@ public void addCustomerJDBC(Customer anCustomer)
         } catch (Exception ex) {
             
            System.out.println(ex.getMessage()); 
+           logger.error(ex.getMessage());
         }
     }
     public Customer getCustomerJDBC(int customerId)
     {
         Customer anCustomer=new Customer();
         try {
+             logger.info("In Business Layer getCustomerJDBC(int customerId) method");
             System.out.println("In Business Layer getCustomerJDBC(int customerId) method");
             Factory factory = new Factory();           
             ICustomerServiceJDBC iCustomerMgrJDBC = (ICustomerServiceJDBC) factory.getTheService(ICustomerServiceJDBC.NAME);
@@ -48,7 +53,7 @@ public void addCustomerJDBC(Customer anCustomer)
             
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
-            
+             logger.error(ex.getMessage());
         }
         return anCustomer;
     }
@@ -56,6 +61,7 @@ public void addCustomerJDBC(Customer anCustomer)
     public void updateCustomerJDBC(Customer anCustomer)
     {
         try {
+            logger.info("In Business Layer  updateCustomerJDBC(Customer anCustomer) method");
             System.out.println("In Business Layer  updateCustomerJDBC(Customer anCustomer) method");
             Factory factory = new Factory();            
             ICustomerServiceJDBC iCustomerMgrJDBC = (ICustomerServiceJDBC) factory.getTheService(ICustomerServiceJDBC.NAME);
@@ -67,6 +73,7 @@ public void addCustomerJDBC(Customer anCustomer)
         } catch (Exception ex) {
             
            System.out.println(ex.getMessage()); 
+            logger.error(ex.getMessage());
         }
     }
     
@@ -75,6 +82,7 @@ public void addCustomerJDBC(Customer anCustomer)
         ResultSet rs=null;
         List<Customer> CustomerList = new ArrayList<>();
         try {
+            logger.info("In Business Layer getAllCustomer JDBC method");
             System.out.println("In Business Layer getAllCustomer JDBC method");
             Factory factory = new Factory();
             ICustomerServiceJDBC iCustomerMgrJDBC = (ICustomerServiceJDBC) factory.getTheService(ICustomerServiceJDBC.NAME);
@@ -84,7 +92,8 @@ public void addCustomerJDBC(Customer anCustomer)
           System.out.println(ex.getMessage());
             
         } catch (Exception ex) {
-            System.out.println(ex.getMessage());            
+            System.out.println(ex.getMessage());   
+             logger.error(ex.getMessage());
         }
         return rs;
     }
@@ -93,6 +102,7 @@ public void addCustomerJDBC(Customer anCustomer)
     public void deleteCustomerJDBC(int customerId)
     {
         try {
+            logger.info("In Business Layer delete Customer JDBC method");
             System.out.println("In Business Layer delete Customer JDBC method");
             Factory factory = new Factory();
             ICustomerServiceJDBC iCustomerMgrJDBC = (ICustomerServiceJDBC) factory.getTheService(ICustomerServiceJDBC.NAME);
@@ -104,6 +114,7 @@ public void addCustomerJDBC(Customer anCustomer)
         } catch (Exception ex) {
             
             System.out.println(ex.getMessage());
+             logger.error(ex.getMessage());
         }
     }
 
